daacdd8fb0f93d25187fc6ec6e54d4fb9c54a2fc
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2024-07-05 13:47:31 +0200

    UICollectionView - Follower Cell UI

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Antoine Goncalves on 05/07/2024.
+//
+
+import UIKit
+
+enum GFError: String, Error {
+    case invalidUsername    = "This username created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalideResponse   = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again"
+}
