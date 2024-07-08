9dfaf8af45273ecbbbf5fe234a94ca822f7c39b8
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2024-07-08 14:56:06 +0200

    Network Call - Get User Info

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Antoine Goncalves on 08/07/2024.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
