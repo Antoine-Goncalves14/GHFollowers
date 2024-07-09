dbb0e35654b26e0229fb7e8555f3df9668695743
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2024-07-09 12:11:49 +0200

    Button Functionality - Delegates & Protocols

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol FollowerListVCDelegate: AnyObject {
+    func  didRequestFollowers(for username: String)
+}
+
 class FollowerListVC: UIViewController {
     
     enum Section { case main }
@@ -140,6 +144,7 @@ extension FollowerListVC: UICollectionViewDelegate {
         let destVC        = UserInfoVC()
         
         destVC.username   = follower.login
+        destVC.delegate   = self
         
         let navController = UINavigationController(rootViewController: destVC)
         
@@ -174,4 +179,20 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
         }
     }
     
+}
+
+extension FollowerListVC: FollowerListVCDelegate {
+    
+    func didRequestFollowers(for username: String) {
+        self.username   = username
+        title           = username
+        page            = 1
+        
+        followers.removeAll()
+        filteredFollowers.removeAll()
+        
+        collectionView.setContentOffset(.zero, animated: true)
+        
+        getFollowers(username: username, page: page)
+    }
 }
