522d1c91a268983cb4c7f09ce73461f680db6fcb
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2024-07-09 11:13:20 +0200

    Date Conversion & Date Label

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by Antoine Goncalves on 09/07/2024.
+//
+
+import UIKit
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        
+        let dateFormatter           = DateFormatter()
+        
+        dateFormatter.dateFormat    = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
